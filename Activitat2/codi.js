7ae7815262790e1ac311c4eb8d315dc268e4c7ac
alorca89 <59705337+alorca89@users.noreply.github.com>
2020-02-07 18:36:38 +0100

    Add files via upload

---
+++ Activitat2/codi.js
@@ -0,0 +1,8 @@
+
+//creem una primera alerta amb una salutació
+var salutacio = "Em dic Alba!";
+alert (salutacio)
+
+//creem una segona alerta posterior a la primera
+var salutacio2= "Sóc el segon script";
+alert (salutacio2)
